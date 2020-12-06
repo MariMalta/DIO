ac5a65b8b385deed71e33f2df042f33b3fbab2b2
MariMalta <maltamariana123@gmail.com>
2020-12-05 23:26:02 -0300

    Commit Inicial

--- JS_ES6_Essencial/arrays.js
+++ JS_ES6_Essencial/arrays.js
@@ -1,2 +1,40 @@
 const users = ['Joao','Guilherme','Maria'];
-const 
+const age = {
+    Under_Age: Symbol ('U'),
+    Of_Age: Symbol ('A')
+}
+
+const persons = [
+    {
+        name: 'Joao',
+        lastName: 'Dias',
+        age: age.Of_Age 
+    },
+    {
+        name: 'Guilherme',
+        lastName: 'Freitas',
+        age: age.Under_Age
+    },
+    {
+        name: 'Maria',
+        lastName: 'Cavalcanti',
+        age: age.Of_Age
+    }
+];
+
+console.log('Itens:', persons.length);
+console.log('É Array?', Array.isArray(persons));
+
+persons.forEach((person, index, arr) =>{
+    console.log(`Nome: ${person.name} Index: ${index}`, persons);
+});
+
+const OfAge = persons.filter(person => person.age === age.Of_Age);
+console.log('Lista de pessoa maiores de idade:', OfAge);
+
+const PersonsWithTicket = persons.map(person => {
+    person.Ticket = 'Ingresso Valido';
+    return person;
+});
+console.log(PersonsWithTicket);
+
